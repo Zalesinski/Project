eefea33a041d8328e785d91e25bc403bdbb122a3
Maryan Zalesinski <zalesinsky@gmail.com>
2021-05-19 22:40:56 +0300

    19/05/2021 	modified:   css/style.css 	modified:   exibition.html 	new file:   images/exibition_img1.jpg 	new file:   images/exibition_img2.jpg 	new file:   images/exibition_img3.jpg 	new file:   images/exibition_img4.jpg 	new file:   images/exibition_img5.jpg 	new file:   images/exibition_img6.jpg 	new file:   images/exibition_img7.jpg 	new file:   images/exibition_img8.jpg 	new file:   images/exibition_img9.jpg 	modified:   index.html 	modified:   script.js 	modified:   scss/style.scss

--- script.js
+++ script.js
@@ -4,3 +4,49 @@ $(document).ready(function() {
         $('body').toggleClass('lock');
     });
 });
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    slidesPerView: 3,
+
+    spaceBetween: 50,
+
+    autoplay: {
+        delay: 5000,
+        stopOnLastSlide: false,
+        disableOnInteraction: false,
+        
+    },
+    speed: 600,
+
+    breakpoints: {
+      960: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+      767: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+    }
+
+  });
